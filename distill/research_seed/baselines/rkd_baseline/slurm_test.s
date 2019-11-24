36d937db6d96a00208eb1064f8634d0ff170233c
Karanbir Chahal <karanbleep@gmail.com>
2019-11-23 22:06:12 -0500

    Slurm test

---
+++ distill/research_seed/baselines/rkd_baseline/slurm_test.s
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=seg_test
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32GB
+#SBATCH --time=4:00:00
+#SBATCH --gres=gpu:p40:1
+#SBATCH --output=slurm_%j.out
+
+. ~/.bashrc
+cd /scratch/ksc487/distill/nyu-cv-project/distill/research_seed/baselines/rkd_baseline
+conda activate light
+
+python rkd_baseline_trainer.py --version 1 --path-to-teacher ../no_kd_baseline/lightning_logs/default/version_1/checkpoints/_ckpt_epoch_25.ckpt --epochs 40
