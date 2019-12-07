855d70f7d7f2338259afbd05baad22b9239c99ff
fruffy <fruffy@nyu.edu>
2019-12-07 15:39:19 -0500

    Clean up some of fd, try out some weird loss

--- distillers/fd_distiller.py
+++ distillers/fd_distiller.py
@@ -2,8 +2,6 @@ import torch
 import torch.nn as nn
 from trainer import BaseTrainer
 
-DEPTH = 2
-
 
 def get_layer_types(feat_layers):
     conv_layers = []
@@ -13,7 +11,7 @@ def get_layer_types(feat_layers):
     return conv_layers
 
 
-def get_net_info(net, feats_as_module=False):
+def get_net_info(net, as_module=False):
     device = next(net.parameters()).device
     if isinstance(net, nn.DataParallel):
         net = net.module
@@ -27,25 +25,12 @@ def get_net_info(net, feats_as_module=False):
     for layer in feat_layers:
         x = layer(x)
         channels.append(x.shape[1:])
-    if feats_as_module:
+    if as_module:
         return nn.ModuleList(feat_layers), linear, channels
     return feat_layers, linear, channels
 
 
-def get_layers(layers, x, lasts=[]):
-    layer_feats = []
-    outs = []
-    out = x
-    for layer in layers:
-        out = nn.functional.relu(layer(out))
-        layer_feats.append(out)
-    for last in lasts:
-        out = last(out)
-        outs.append(out)
-    return layer_feats, outs
-
-
-def compute_last_layer(linear, last_channel):
+def set_last_layers(linear, last_channel, as_module=False):
     # assume that h_in and w_in are equal...
     c_in = last_channel[0]
     h_in = last_channel[1]
@@ -53,56 +38,75 @@ def compute_last_layer(linear, last_channel):
     flat_size = c_in * h_in * w_in
     pooling = int((flat_size / linear.in_features)**(0.5))
     modules = [nn.AvgPool2d(pooling), nn.Flatten(), linear]
-    return nn.ModuleList(modules)
+    if as_module:
+        return nn.ModuleList(modules)
+    return modules
+
+
+def get_layers(layers, x, lasts=[], use_relu=True):
+    layer_feats = []
+    outs = []
+    out = x
+    for layer in layers:
+        out = layer(out)
+        if use_relu:
+            out = nn.functional.relu(out)
+        layer_feats.append(out)
+    for last in lasts:
+        out = last(out)
+        outs.append(out)
+    return layer_feats, outs
 
 
 class Distiller(nn.Module):
-    def __init__(self, s_net, t_net, batch_size=128, device="cuda"):
+    def __init__(self, s_net, t_net):
         super(Distiller, self).__init__()
 
         self.s_feat_layers, self.s_linear, s_channels = get_net_info(
-            s_net, True)
-        self.t_feat_layers, self.t_linear, t_channels = get_net_info(t_net)
-        self.s_last = compute_last_layer(self.s_linear, s_channels[-1])
-        self.t_last = compute_last_layer(self.t_linear, t_channels[-1])
-        # freeze the teacher completely
-        for t_layer in self.t_feat_layers:
-            t_layer.requires_grad = False
-        for t_layer in self.t_last:
-            t_layer.requires_grad = False
+            s_net, as_module=True)
+        self.t_feat_layers, self.t_linear, t_channels = get_net_info(
+            t_net, as_module=False)
+        self.s_last = set_last_layers(
+            self.s_linear, s_channels[-1], as_module=True)
+        self.t_last = set_last_layers(
+            self.t_linear, t_channels[-1], as_module=False)
 
     def compute_feature_loss(self, s_feats, t_feats):
         feature_loss = 0.0
-        depth = len(t_feats)
+        t_depth = len(t_feats)
+        s_depth = len(s_feats)
+        diff = t_depth - s_depth
+        if diff < 0:
+            offset = 0
+            depth = t_depth
+        else:
+            offset = diff
+            depth = s_depth
         for idx in range(depth):
-            s_idx = idx
-            if idx > (len(s_feats) - 1):
-                s_idx = len(s_feats) - 1
-            s_feat = s_feats[s_idx]
-            t_feat = t_feats[idx]
+            s_feat = s_feats[idx]
+            t_feat = t_feats[idx + offset]
             s_c = s_feat.shape[1]
-            s_h = s_feat.shape[2]
-            s_w = s_feat.shape[3]
-            t_c = t_feat.shape[1]
-            t_h = t_feat.shape[2]
-            t_w = t_feat.shape[3]
-            if t_c > s_c:
-                c_ratio = t_c / s_c
-                h = int(c_ratio * t_h / 2)
-                t_feat = t_feat.view(t_feat.shape[0], s_c, h, -1)
-                out_shape = (s_h, s_w)
-                t_feat = nn.functional.adaptive_avg_pool2d(t_feat, out_shape)
-
-            feature_loss += nn.functional.mse_loss(s_feat, t_feat)
-        return feature_loss
+            t_feat = t_feat.view(t_feat.shape[0], s_c, -1)
+            s_feat = s_feat.view(s_feat.shape[0], s_c, -1)
+            if t_feat.shape[2] > s_feat.shape[2]:
+                out_shape = s_feat.shape[2]
+                t_feat = nn.functional.adaptive_avg_pool1d(t_feat, out_shape)
+            else:
+                out_shape = t_feat.shape[2]
+                s_feat = nn.functional.adaptive_avg_pool1d(s_feat, out_shape)
+            s_feat = s_feat.view(s_feat.shape[0], -1)
+            t_feat = t_feat.view(t_feat.shape[0], -1)
+            feature_loss += nn.functional.triplet_margin_loss(
+                s_feat, t_feat, s_feat)
+        return feature_loss / 10000
 
     def forward(self, x, targets=None, is_loss=False):
-        s_feats, s_outs = get_layers(self.s_feat_layers, x, self.s_last)
-        t_feats, t_outs = get_layers(self.t_feat_layers, x, self.t_last)
+        s_feats, s_outs = get_layers(self.s_feat_layers, x, self.s_last, False)
+        t_feats, t_outs = get_layers(self.t_feat_layers, x, self.t_last, False)
+        feature_loss = 0.0
+        feature_loss += self.compute_feature_loss(s_feats, t_feats)
+        _, s_outs = get_layers(self.s_feat_layers, x, self.s_last)
         if is_loss:
-            feature_loss = 0.0
-            feature_loss += self.compute_feature_loss(s_feats, t_feats)
-            feature_loss += nn.functional.mse_loss(s_outs[-1], t_outs[-1])
             return s_outs[-1], feature_loss
         return s_outs[-1]
 
@@ -127,8 +131,8 @@ class FDTrainer(BaseTrainer):
 
     def calculate_loss(self, data, targets):
         s_out, feature_loss = self.net(data, targets, is_loss=True)
-        loss_CE = self.loss_fun(s_out, targets)
-        loss = feature_loss + loss_CE
+        loss_ce = self.loss_fun(s_out, targets)
+        loss = loss_ce + feature_loss
         loss.backward()
         self.optimizer.step()
         return s_out, loss
